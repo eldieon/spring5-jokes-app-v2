9453d3dc10d37290ced81bd31805823249a5ac8b
eldieon <vanessachanliau@Vanessas-MacBook-Air.local>
2023-12-06 11:38:00 +0100

    clean up

--- src/main/java/controllers/JokesController.java
+++ src/main/java/controllers/JokesController.java
@@ -3,23 +3,20 @@ package controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import services.JokeService;
-import services.JokeServiceImpl;
 
 @Controller
 public class JokesController {
     private final JokeService jokeService;
 
-    @Autowired
     public JokesController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
     @RequestMapping({"/", ""})
-    public String showJoke(Model model){
-        System.out.println("get joke!");
+    public String showJoke(Model model) {
+
         model.addAttribute("joke", jokeService.getJoke());
 
         return "index";
--- src/main/java/services/JokeServiceImpl.java
+++ src/main/java/services/JokeServiceImpl.java
@@ -5,20 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JokeServiceImpl implements JokeService{
+public class JokeServiceImpl implements JokeService {
 
-//create a service to return a jokes string from class "ChuckNorrisQuotes.getRandomQuote()"
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
     public JokeServiceImpl() {
         this.chuckNorrisQuotes = new ChuckNorrisQuotes();
-        System.out.println(chuckNorrisQuotes.getRandomQuote());
     }
 
     @Override
     public String getJoke() {
-        System.out.println("getting joke!");
-        String joke = chuckNorrisQuotes.getRandomQuote();
-        return joke;
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
